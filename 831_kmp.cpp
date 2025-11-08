6b6204f252f5d73492edc79a560b934100f92104
lumengovo <13621966855@163.com>
2025-11-08 17:28:57 +0800

    Add files via upload

--- 831_kmp.cpp
+++ 831_kmp.cpp
@@ -1,44 +1,34 @@
 #include <iostream>
+
 using namespace std;
 
 const int N = 100010, M = 1000010;
-char p[N], s[M];
+
+int n, m;
 int ne[N];
+char s[M], p[N];
 
 int main()
 {
-	int n, m;
-	cin >> n >> p + 1 >> m >> s + 1;
-	ne[1] = 0;
-	ne[0] = 0;
-	//next数组
-	for (int i = 2, j = 1; i <= n; i++)
-	{
-	    if (j == 0) j = 1; 
-		while (j > 1 && p[i] != p[j]) 
-		{
-            j = ne[j-1];
-            if (j == 0) j = 1;  
-     }
-		if (p[i] == p[j]) j++;
-		ne[i] = j;
-	}
-
-	//kmp匹配
-	for (int i = 1, j = 1; i <= m; i++)
-	{
-	    if (j == 0) j = 1; 
-		while (j > 1 && s[i] != p[j]) 
-		{
-            j = ne[j-1];
-            if (j == 0) j = 1;  
+    cin >> n >> p + 1 >> m >> s + 1;
+    
+    for (int i = 2, j = 0; i <= n; i ++ )
+    {
+        while (j && p[i] != p[j + 1]) j = ne[j];
+        if (p[i] == p[j + 1]) j ++ ;
+        ne[i] = j;
+    }
+    
+    for (int i = 1, j = 0; i <= m; i ++ )
+    {
+        while (j && s[i] != p[j + 1]) j = ne[j];
+        if (s[i] == p[j + 1]) j ++ ;
+        if (j == n)
+        {
+            printf("%d ", i - n);
+            j = ne[j];
         }
-		if (s[i] == p[j]) j++;
-		if (j == n+1)
-		{
-			printf("%d ", i - n+1);
-			j = ne[j-1];
-		}
-	}
-	return 0;
+    }
+    
+    return 0;
 }
