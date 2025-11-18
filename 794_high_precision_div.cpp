1fc66152bbd644a674ebf73f7bb87406e9e780f9
lumengovo <13621966855@163.com>
2025-11-18 14:11:09 +0800

    Add files via upload

---
+++ 794_high_precision_div.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> div(vector<int> &A , int b, int &r)
+{
+    vector<int> C;
+    for(int i = A.size() - 1 ; i >= 0 ; i--)
+    {
+        r = r * 10 + A[i];
+        C.push_back(r / b);
+        r = r % b;
+    }
+    reverse(C.begin(), C.end());
+    while(C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
+}
+
+int main()
+{
+    string a;
+    int b;
+    int r = 0;//余数
+    vector<int> A;
+    cin >> a >> b;
+    for(int i = a.size() - 1 ; i >= 0 ; i--) A.push_back(a[i] - '0');
+    vector<int> C = div(A , b, r);
+    for(int i = C.size() - 1; i >= 0 ; i--) cout << C[i];
+    cout << endl << r << endl;
+    return 0;
+}
